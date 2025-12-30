e585b2c2f75e91cbbcf043b141fbc775f719e9a8
SSOL <msseock@gmail.com>
2025-12-30 14:20:15 +0900

    ✨ Feat. 시작 프로젝트 생성
    스토리보드를 활용하는 프로젝트를 생성했습니다

---
+++ SeSACStart/SeSACStart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACStart
+//
+//  Created by 석민솔 on 12/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
