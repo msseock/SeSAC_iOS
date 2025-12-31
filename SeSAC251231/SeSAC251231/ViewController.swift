18ba5c3d6e6c02a2fde373e3a5d3f71067f7535c
SSOL <msseock@gmail.com>
2025-12-31 17:13:36 +0900

    🎨 Storyboard: 3일차 스토리보드 실습
    - 컨테이너 뷰 컨트롤러(NavigationController & TabBarController)
    - AutoLayout

---
+++ SeSAC251231/SeSAC251231/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC251231
+//
+//  Created by 석민솔 on 12/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
