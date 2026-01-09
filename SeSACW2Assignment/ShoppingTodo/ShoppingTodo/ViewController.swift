8d162f2c3dfdd2c813e07b68649b6b60f6426566
SSOL <msseock@gmail.com>
2026-01-09 18:22:17 +0900

    🎉 UIKit: 2주차 과제 프로젝트 생성

---
+++ SeSACW2Assignment/ShoppingTodo/ShoppingTodo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingTodo
+//
+//  Created by 석민솔 on 1/9/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
